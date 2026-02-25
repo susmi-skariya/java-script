e3e4222893c10a3def5eb830fa3dc3c66943bf86
susmi-skariya <susmiskariya329@gmail.com>
2026-02-25 17:43:56 +0530

    javascript variables and data types

---
+++ variable.js
@@ -0,0 +1,44 @@
+// variables
+
+// var a=10;
+// var a=13;
+// console.log(a)
+
+let b=3;
+b=4; 
+console.log(typeof(b));
+
+const PI=3.14;
+console.log(PI)
+
+// Data types
+let name="susmi";
+console.log(typeof(name));
+
+let age=10;
+console.log(age);
+
+let d=true;
+console.log(d);
+
+let lastname;
+console.log(lastname);
+
+let f=null;
+console.log(typeof(f));
+
+let person={
+    name:"susmi",
+    age:"20"
+}
+console.log(typeof(person));
+
+let m=BigInt(12345678891523567)
+console.log(m);
+
+let a=Symbol("susmi")
+let h=Symbol("susmi")
+console.log(a==h)
+
+
+
